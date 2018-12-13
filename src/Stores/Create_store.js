6fdf4413415210170cf06940d54ebb4d4154a8ff
irion94 <tomasz.kozub218@gmail.com>
2018-12-13 20:47:40 +0100

    Front: mobX stores for Create Tab, Create tab with forms

--- src/Stores/Create_store.js
+++ src/Stores/Create_store.js
@@ -2,26 +2,14 @@ import {action, observable} from "mobx";
 import {map} from 'ramda'
 
 class Create_store {
-    // @observable person = {
-    //     name: ''
-    // };
-    //
-    // @observable vehicle = {
-    //     brand: ''
-    // };
-
     @observable personalities = {
-        person: {
-        },
-        vehicle: {
-        }
-    }
+        person: {},
+        vehicle: {}
+    };
 
     @action reset = (key) => {
-        this.personalities[key] = {
-
-        }
-    }
+        this.personalities[key] = {}
+    };
 
     @action setPersonalities = (input, key) => {
         this.personalities[key] = input;
@@ -37,30 +25,6 @@ class Create_store {
             return inline
         }
     }
-
-    // @action setPerson = (input) => {
-    //     this.person = {...this.person, ...input};
-    // };
-    //
-    // @action getPerson = (method) => {
-    //     if (method === 'object') {
-    //         return this.person
-    //     }
-    //     else if (method === 'inline') {
-    //         let inline = '';
-    //         map((item) => inline = inline.concat(" ", item), this.person);
-    //         return inline
-    //     }
-    // };
-    //
-    // @action setVehicle = (input) => {
-    //     this.vehicle = {...this.vehicle, ...input}
-    // };
-    //
-    // @action getVehicle = () => {
-    //     return this.vehicle
-    // }
-
 }
 
 let createStore = new Create_store();
